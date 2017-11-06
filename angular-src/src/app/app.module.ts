7c5f5169d8ff91b7da54f9e5af54feb32e6af4dd
mihail.seregin <mihail.seregin@itechart-group.com>
2017-11-06 13:51:41 +0300

    components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AppComponent } from './app.component';
+import { ValidateService } from "./services/validate.service";
+import { FlashMessagesModule } from "angular2-flash-messages";
 
-import {NavigationComponent} from "./navigation/nav.component";
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+    ValidateService
+  ],
   bootstrap: [
-    NavigationComponent
+    AppComponent
   ]
 })
 export class AppModule { }
---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from "../../services/validate.service";
+import { FlashMessagesService } from "angular2-flash-messages";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateService: ValidateService, private flashMessageService: FlashMessagesService) { }
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessageService.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessageService.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+  }
+
+}
